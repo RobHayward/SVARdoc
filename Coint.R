43f29fc161c9609f384c386d1fbba0d68104c626
RobHayward <robohay@gmail.com>
2013-06-27 22:24:29 +0100

    Updated loop for unit root tests.

--- Coint.R
+++ Coint.R
@@ -11,14 +11,27 @@ ds <- da[,c(3:7,9,11)]
 ds <- da[,c(13, 4:7, 9, 11)]
 head(ds)
 # Test Unit Root--------------------------
-M <- matrix(0, nrow = 2, ncol = 8)
-summary(ur.df(ds[,i], type = "trend", selectlags = "AIC"))
-
-
-
-summary(ur.df(ds[,5], type = "trend", selectlags = "AIC"))
+require(xtable)
+M <- matrix(0, nrow = 4, ncol = 7)
+par(mfrow = c(3,2))
+for(i in 1:7){
+  title <- paste("Plot of ",colnames(ds)[i], sep = "")
+  plot(ds[,i], type = 'l', main = title)
+  a <- ur.df(ds[,i], type = "trend", selectlags = "AIC")
+  a@cval
+  M[1,i] <- a@teststat[1,1] 
+  M[2,i] <- a@cval[1,2]
+  M[3,i] <- a@teststat[1,3]
+  M[4,i] <- a@cval[3,2]
+}
+colnames(M) <- c("CNB", "CNE", "CNFDI", "COT", "RTWI", "SPREAD2", "S1")
+rownames(M) <- c("tau", "CV(tau)", "phi2", "CV(phi)")
+#print(xtable(M), title = "Critical Values for DF Test, digits = 4")
+M
 # apply(da, MARGIN = 2, FUN = ur.df, type = "trend", selectlags = c("AIC"))
 plot(ds[,5], type = 'l')
+
+
 # Creat new dataframe for differenced----------------------------
 # After finding that CNB, RTI and SPREAD1 appear to have unit roots, 
 # will be differenced to make them stationary. 
