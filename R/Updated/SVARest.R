e6a1bf40a5b64beb3bf829d34b651d1e07664166
RobHayward <robohay@gmail.com>
2020-04-12 10:18:58 +0100

    This has been prepared for Sending to Economic Letters

--- R/Updated/SVARest.R
+++ R/Updated/SVARest.R
@@ -5,7 +5,7 @@ Svar1$A
 #summary(Svar1)
 #Svar2<-SVAR(Var2,estmethod='direct',Amat=Amat,hessian=TRUE)
 #Svar2$A
-#Svar3<-SVAR(Var3,estmethod='direct',Amat=Amat,hessian=TRUE)
+Svar3<-SVAR(Var3,estmethod='direct',Amat=Amat,hessian=TRUE)
 #Svar3$A
 # This is the A matrix
 #if you want turn list into dataframe and xtable for la tex
--- R/Updated/TotalUpdate.R
+++ R/Updated/TotalUpdate.R
@@ -8,11 +8,11 @@ rm(info)
 source('R/Updated/SVAR.R')
 source('R/Updated/SVARest.R')
 summary(Svar1)
-pdf("./Figures3/NewIRF.pdf", paper= "a4r", width = 9, title = "IRF1")
+pdf("./Figures3/NewIRFb.pdf", paper= "a4r", width = 9, title = "IRF1")
 # Need to make consistent with others so made in irf3.R
 par(mfcol=c(2,3), oma = c(0,0,1,0))
 myPlot <- function(imp){
-plot(unlist(imp[[1]], seq(0, 9, 1)), type = 'l', ylim = c(-1, 3), 
+plot(unlist(imp[[1]], seq(0, 9, 1)), type = 'l', ylim = c(-2, 4), 
      main = paste("One unit ", names(imp[[1]]), " innovation", sep = ""), 
      xlab = "Quarters", ylab = "RTWI", lwd = 2)
 lines(unlist(imp[[2]]), type = 'l', lty = 2)
@@ -22,11 +22,11 @@ abline(h = 0)
 capital_flow <- c("Bond", "Equity", "ER", "CB", "Irs", "Spec")
 imp <- list(length = length(capital_flow))
 for(i in capital_flow){
-  imp[[i]] <- irf(Svar1, cumulative = T, impulse = i, response = "ER", boot = "TRUE", 
+  imp[[i]] <- irf(Svar3, cumulative = T, impulse = i, response = "ER", boot = "TRUE", 
                 runs = 100, n.ahead = 3)
 }
 #par(mfrow = c(2, 3))
 for(i in capital_flow){
   myPlot(imp[[i]])
 }
-
+dev.off()
